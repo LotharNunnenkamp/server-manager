04189705653b0ea92386a1534a403eff145fd4f2
Lothar <lothar223@hotmail.com>
2025-01-16 09:53:09 -0300

    feat(service): method create server

--- back-end/server-manager/src/main/java/com/example/server_manager/service/ServerServiceImpl.java
+++ back-end/server-manager/src/main/java/com/example/server_manager/service/ServerServiceImpl.java
@@ -1,14 +1,20 @@
 package com.example.server_manager.service;
 
+import com.example.server_manager.enums.Status;
 import com.example.server_manager.model.Server;
 import com.example.server_manager.repository.ServerRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.net.InetAddress;
 import java.util.Collection;
 import java.util.List;
 
 @Service
+@Slf4j
+@Transactional
 public class ServerServiceImpl implements ServerService {
 
     @Autowired
@@ -16,7 +22,9 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
     }
 
     @Override
@@ -41,6 +49,10 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImageUrl() {
         return null;
     }
 }
