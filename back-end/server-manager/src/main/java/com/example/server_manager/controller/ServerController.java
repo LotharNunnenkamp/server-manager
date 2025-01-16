a0d290afca3d745dceddf8d86b16ac9407f7e69b
Lothar <lothar223@hotmail.com>
2025-01-16 14:22:48 -0300

    feat(controller): create pingServer

--- back-end/server-manager/src/main/java/com/example/server_manager/controller/ServerController.java
+++ back-end/server-manager/src/main/java/com/example/server_manager/controller/ServerController.java
@@ -1,14 +1,18 @@
 package com.example.server_manager.controller;
 
+import com.example.server_manager.enums.Status;
 import com.example.server_manager.model.Response;
+import com.example.server_manager.model.Server;
 import com.example.server_manager.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -26,6 +30,20 @@ public class ServerController {
                         .timeStamp(LocalDateTime.now())
                         .data(Map.of("servers", serverService.list(30)))
                         .message("Servers retrieved")
+                        .httpStatus(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", server))
+                        .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
                         .httpStatus(HttpStatus.OK)
                         .statusCode(HttpStatus.OK.value())
                         .build()
