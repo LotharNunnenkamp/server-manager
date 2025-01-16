d2ae7ed4cc26af707ad2d40d0aaff51c4c5ee2ae
Lothar <lothar223@hotmail.com>
2025-01-16 09:45:00 -0300

    feat(service): create ServerService and Implementation

---
+++ back-end/server-manager/src/main/java/com/example/server_manager/service/ServerServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.server_manager.service;
+
+import com.example.server_manager.model.Server;
+import com.example.server_manager.repository.ServerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class ServerServiceImpl implements ServerService {
+
+    @Autowired
+    private ServerRepository serverRepository;
+
+    @Override
+    public Server create(Server server) {
+        return null;
+    }
+
+    @Override
+    public Server ping(String ipAddress) {
+        return null;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        return List.of();
+    }
+
+    @Override
+    public Server get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+}
