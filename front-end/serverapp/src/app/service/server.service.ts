946636f242a07dc4e80a25d0caf485dd806dc95c
Lothar <lothar223@hotmail.com>
2025-01-20 10:35:42 -0300

    feat: save server

--- front-end/serverapp/src/app/service/server.service.ts
+++ front-end/serverapp/src/app/service/server.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CustomResponse } from '../interface/custom-response';
 import { catchError, Observable, tap, throwError } from 'rxjs';
+import { Server } from '../interface/server';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,14 @@ export class ServerService {
 
   ngOnInit(): void {
     this.servers$ = this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+    .pipe(
+      tap(console.log),
+      catchError(this.handleError)
+    );
+  }
+
+  save = (server: Server): Observable<CustomResponse> =>{
+    return this.http.post<CustomResponse>(`${this.apiUrl}/server/save`, server)
     .pipe(
       tap(console.log),
       catchError(this.handleError)
