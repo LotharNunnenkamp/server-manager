7f9e37212e4b51675e849c16565cc7356837cda8
Lothar <lothar223@hotmail.com>
2025-01-20 14:30:53 -0300

    feat: cors configuration

--- back-end/server-manager/src/main/java/com/example/server_manager/ServerManagerApplication.java
+++ back-end/server-manager/src/main/java/com/example/server_manager/ServerManagerApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class ServerManagerApplication {
@@ -62,6 +68,27 @@ public class ServerManagerApplication {
 							.status(Status.SERVER_DOWN)
 							.build());
 		};
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(List.of(
+				"Origin", "Content-Type", "Accept", "Authorization",
+				"X-Requested-With", "Access-Control-Request-Method",
+				"Access-Control-Request-Headers", "Access-Control-Allow-Origin"));
+		corsConfiguration.setExposedHeaders(List.of(
+				"Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"
+		));
+		corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 
 }
