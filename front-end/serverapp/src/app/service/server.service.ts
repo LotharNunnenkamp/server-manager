b7bf52f03ebcd4b352a7d6ad3a7ffc959eb47313
Lothar <lothar223@hotmail.com>
2025-01-20 10:30:51 -0300

    feat: get server list

--- front-end/serverapp/src/app/service/server.service.ts
+++ front-end/serverapp/src/app/service/server.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CustomResponse } from '../interface/custom-response';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
 
-  constructor() { }
+  private readonly apiUrl = 'any';
+
+  servers$: Observable<CustomResponse> | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.servers$ = this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+    .pipe(
+      tap(console.log),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(handleError: any): Observable<never> {
+    return throwError(() => new Error('Method not implemented.'));
+  }
 }
